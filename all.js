16adac715a46afdd93725e3ab956500a83c4041e
GitHubUrara <nmtr.4oo@gmail.com>
2016-08-01 13:54:15 +0900

    updated

--- all.js
+++ all.js
@@ -1,3 +1,9 @@
-$('#all').on('change', function() {
-    $('input[name=check]').prop('checked', this.checked);
-});
+// $('#all').on('change', function() {
+//     $('input[name=check]').prop('checked', this.checked);
+// });
+
+var count;
+function BoxChecked(check){
+	for(count = 0; count < document.form1.r1.length; count++){
+		document.form1.r1[count].checked = check;	
+	}
